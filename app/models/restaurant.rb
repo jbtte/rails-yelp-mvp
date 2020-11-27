92c5633d2d39b0f6e8b39a22f211fe1d8c18647b
Joao Baumotte <jbaumotte@gmail.com>
2020-11-27 12:19:49 -0300

    Models validations OK

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,13 +1,7 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates :name, :address, presence: true
-  validates :category, presence: true, if: :valid_category?
+  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 
-  private
-
-  def valid_category?
-    categories = ["chinese", "italian", "japanese", "french", "belgian"]
-    categories.include? :category
-  end
 end
